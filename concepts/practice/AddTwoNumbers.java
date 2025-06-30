b04e3de498f88ab21028f30329e02a1237b82249
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-06-30 14:48:44 +0530

    feat(practice): Add Program to add 2 numbers

---
+++ concepts/practice/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+public class AddTwoNumbers {
+    static void sum(int a, int b) {
+        System.out.println("Sum:" + (a + b));
+    }
+
+    public static void main(String[] args) {
+        sum(10, 20);
+    }
+}
