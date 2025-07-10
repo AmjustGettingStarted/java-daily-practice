3f24c8f988cdb397414c6ac81b6ead47951d2982
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-07-10 21:30:39 +0530

    feat(gfg): Program to write inside a file
    Co-authored-by: HARSHITH-MV <harshithmv2002@gmail.com>

---
+++ gfg/File Handling/WriteFile.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+
+public class WriteFile {
+    public static void main(String[] args) {
+        try{
+            FileWriter fw = new FileWriter("car.txt");
+            fw.write("Hello World!!!");
+            fw.close();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
