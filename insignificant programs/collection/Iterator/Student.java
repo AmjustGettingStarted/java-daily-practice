dca725871c88b3472538a997ab947fcdf46bc70d
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-09-15 23:10:08 +0530

    update iterator eg program

---
+++ insignificant programs/collection/Iterator/Student.java
@@ -0,0 +1,13 @@
+package insignificant-programs.collection.Iterator;
+
+public class Student {
+
+    String name;
+    int age;
+
+    Student(String name, int age) {
+
+        this.name = name;
+        this.age = age;
+    }
+}
