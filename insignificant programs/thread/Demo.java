82ba011075a35b8245741974f822b700c91a4832
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-09-16 21:24:40 +0530

    update thread demo program
    Co-authored-by: HARSHITH-MV <harshithmv2002@gmail.com>

--- insignificant programs/thread/Demo.java
+++ insignificant programs/thread/Demo.java
@@ -1,8 +1,21 @@
-
 public class Demo {
     public static void main(String[] args) {
         Thread t1 = new Thread("jeff");
         System.out.println("Name: " + t1.getName() + "Id: " + t1.getId());
+
+        Thread t2 = new Thread(new Car(), "Car1");
+        t2.start();
+
+        String ans = new Thread(new Car(), "Car2 is the name").getName();
+        System.out.println(ans);
     }
 
+}
+
+class Car extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Car is vroom vroom");
+
+    }
 }
