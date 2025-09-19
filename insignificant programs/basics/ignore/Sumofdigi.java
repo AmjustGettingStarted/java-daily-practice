e6676573a9ea746a245ff67e57454f34c80e9a27
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-09-19 22:11:20 +0530

    sum of the digits present in a number

--- insignificant programs/basics/ignore/Sumofdigi.java
+++ insignificant programs/basics/ignore/Sumofdigi.java
@@ -1,6 +1,11 @@
 class Sumofdigi{
     public static void main(String[] args) {
-        
+        int n = 1111;
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
     }
 
 }
